46dffded8ab5f4c9c6989808ff73557478f26929
Holger Kohr <kohr@kth.se>
2015-11-18 22:43:33 +0100

    Remove superfluous version number

--- odl/__init__.py
+++ odl/__init__.py
@@ -25,7 +25,7 @@ to be used to write general code and faciliate code reuse.
 
 from __future__ import absolute_import
 
-__version__ = '0.9b1'
+
 __all__ = ('diagnostics', 'discr', 'operator', 'set', 'space', 'solvers')
 
 
